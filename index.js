91d525472b45f85f318547653f7dd0146bef3129
rashedsabbir <rashedulhasan1996@gmail.com>
2022-09-03 20:31:55 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const cors = require("cors");
-
+const errorHandler = require('./MiddleWare/errorHandler');
 const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(errorHandler);
 
 
 app.get("/", (req, res) => {
-    res.send("server is running");
+    res.send("Random user server is running successfully");
   });
 
   app.listen(port, () => {
