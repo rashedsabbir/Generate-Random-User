edc25975c915acf24206cb97efc687c735d3bc11
rashedsabbir <rashedulhasan1996@gmail.com>
2022-09-03 21:03:31 +0600

    userdata added

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const errorHandler = require('./MiddleWare/errorHandler');
 const port = process.env.PORT || 5000;
-
+const fs = require("fs");
+const path = require("path")
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(errorHandler);
 
 
+process.on("unhandledRejection", (error) => {
+    console.log(error.name, error.message);
+    app.close(() => {
+      process.exit(1);
+    });
+  });
+
 app.get("/", (req, res) => {
     res.send("Random user server is running successfully");
+  });
+
+  app.all("*", (req, res) => {
+    res.send("Looks like you've found the doorway to the great nothing.");
   });
 
   app.listen(port, () => {
