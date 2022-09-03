5cd0b55981d9952cc0ac4cf04614e2b427d67117
rashedsabbir <rashedulhasan1996@gmail.com>
2022-09-03 20:17:04 +0600

    port setup done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+    res.send("server is running");
+  });
+
+  app.listen(port, () => {
+    console.log(`Random user-api app listening on port ${port}`);
+  });
